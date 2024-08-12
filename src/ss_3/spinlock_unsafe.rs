61e84b2b8d82487e00d4211ed0821bac58e7821c
aki <aki@mymail.com>
2024-08-13 01:58:26 +0900

    spinlockの例

---
+++ src/ss_3/spinlock_unsafe.rs
@@ -0,0 +1,19 @@
+use rust_atomic_lock::spinlock_unsafe::SpinLock;
+use std::thread;
+
+fn main() {
+    let x = SpinLock::new(Vec::new());
+    thread::scope(|s| {
+        s.spawn(|| {
+            x.lock().push(1);
+        });
+        s.spawn(|| {
+            let mut g = x.lock();
+            g.push(2);
+            g.push(2);
+        });
+    });
+
+    let g = x.lock();
+    assert!(g.as_slice() == [1, 2, 2] || g.as_slice() == [2, 2, 1]);
+}
