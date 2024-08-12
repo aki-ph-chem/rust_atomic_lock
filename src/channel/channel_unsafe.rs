1f3bdac7c5547788be5b9e3efea867c224548c99
aki <aki@mymail.com>
2024-08-13 04:57:39 +0900

    とりあえずunsafeなapiで実装した

--- src/channel/channel_unsafe.rs
+++ src/channel/channel_unsafe.rs
@@ -1,6 +1,9 @@
 use std::cell::UnsafeCell;
 use std::mem::MaybeUninit;
-use std::sync::atomic::AtomicBool;
+use std::sync::atomic::{
+    AtomicBool,
+    Ordering::{Acquire, Release},
+};
 
 pub struct Channel<T> {
     message: UnsafeCell<MaybeUninit<T>>,
@@ -8,3 +11,29 @@ pub struct Channel<T> {
 }
 
 unsafe impl<T> Sync for Channel<T> where T: Send {}
+
+impl<T> Channel<T> {
+    pub fn new() -> Self {
+        Self {
+            message: UnsafeCell::new(MaybeUninit::uninit()), // チャネルの内容は空で初期化
+            ready: AtomicBool::new(false),
+        }
+    }
+
+    /// 一度しか読んではならない
+    pub unsafe fn send(&self, message: T) {
+        (*self.message.get()).write(message);
+        self.ready.store(true, Release);
+    }
+
+    pub fn is_ready(&self) -> bool {
+        self.ready.load(Acquire)
+    }
+
+    /// `is_read`がtrueを返す場合にのみ呼ぶこと
+    ///
+    /// 一度しか読んではならない
+    pub unsafe fn receive(&self) -> T {
+        (*self.message.get()).assume_init_read()
+    }
+}
