9ab22740d3e1431e79aff08354984ccc2ff8693c
aki <aki@mymail.com>
2024-08-13 15:45:24 +0900

    ワンショットチャンネルの例

---
+++ src/ss_5/channel_one_shot.rs
@@ -0,0 +1,22 @@
+use rust_atomic_lock::channel::channel_unsafe::Channel;
+use std::thread;
+
+fn main() {
+    let channel = Channel::<&str>::new();
+    // t: 受信スレッド(メインスレッド)
+    let t = thread::current();
+
+    thread::scope(|s| {
+        s.spawn(|| {
+            channel.send("Hello World");
+            // 一度メッセージを送ったらメインスレッドをunparkする
+            t.unpark();
+        });
+
+        // 受信スレッド(メインスレッド)はpark()で受信するまで待機
+        while !channel.is_ready() {
+            thread::park();
+        }
+        assert_eq!(channel.receive(), "Hello World");
+    });
+}
