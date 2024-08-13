22e27c41f4cb2e586d5f9ce9e84672e292c12ff0
aki <aki@mymail.com>
2024-08-13 13:37:22 +0900

    インターフェースをsafeにした

--- src/channel/channel_unsafe.rs
+++ src/channel/channel_unsafe.rs
@@ -2,11 +2,12 @@ use std::cell::UnsafeCell;
 use std::mem::MaybeUninit;
 use std::sync::atomic::{
     AtomicBool,
-    Ordering::{Acquire, Release},
+    Ordering::{Acquire, Relaxed, Release},
 };
 
 pub struct Channel<T> {
     message: UnsafeCell<MaybeUninit<T>>,
+    in_use: AtomicBool,
     ready: AtomicBool,
 }
 
@@ -16,24 +17,35 @@ impl<T> Channel<T> {
     pub fn new() -> Self {
         Self {
             message: UnsafeCell::new(MaybeUninit::uninit()), // チャネルの内容は空で初期化
+            in_use: AtomicBool::new(false),
             ready: AtomicBool::new(false),
         }
     }
 
-    /// 一度しか読んではならない
-    pub unsafe fn send(&self, message: T) {
-        (*self.message.get()).write(message);
+    /// 二つ以上のメッセージを送信しようとしたらpanic
+    pub fn send(&self, message: T) {
+        if self.in_use.swap(true, Relaxed) {
+            panic!("can't send more than one message");
+        }
+
+        unsafe { (*self.message.get()).write(message) };
         self.ready.store(true, Release);
     }
 
     pub fn is_ready(&self) -> bool {
-        self.ready.load(Acquire)
+        self.ready.load(Relaxed)
     }
 
-    /// `is_read`がtrueを返す場合にのみ呼ぶこと
+    /// メッセージが空ならpanic
+    /// メッセージがすでに読み込まれていてもpanic
     ///
-    /// 一度しか読んではならない
-    pub unsafe fn receive(&self) -> T {
-        (*self.message.get()).assume_init_read()
+    /// `is_read`がtrueを返す場合にのみ呼ぶこと
+    pub fn receive(&self) -> T {
+        if !self.ready.swap(true, Acquire) {
+            panic!("no message available");
+        }
+
+        // 安全性: readyをチェックしてフラッグをリセットした
+        unsafe { (*self.message.get()).assume_init_read() }
     }
 }
