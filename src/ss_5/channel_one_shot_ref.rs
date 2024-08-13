aadff3edbe4d4a1fb156ea7b10e7f9e7d3e647bf
aki <aki@mymail.com>
2024-08-13 22:39:05 +0900

    ブロッキングまで実装した例

--- src/ss_5/channel_one_shot_ref.rs
+++ src/ss_5/channel_one_shot_ref.rs
@@ -5,15 +5,9 @@ fn main() {
     let mut channel = Channel::<&str>::new();
     thread::scope(|s| {
         let (sender, receiver) = channel.split();
-        let t = thread::current();
-
         s.spawn(move || {
             sender.send("Hello World");
-            t.unpark();
         });
-        while !receiver.is_ready() {
-            thread::park();
-        }
 
         assert_eq!(receiver.receive(), "Hello World");
     });
