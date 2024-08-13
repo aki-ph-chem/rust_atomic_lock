0cb4204d0226b7c476afbf3658c1b67f8b764240
aki <aki@mymail.com>
2024-08-13 22:35:14 +0900

    unpakr,parkをchannel側に実装

--- src/channel/channel_ref.rs
+++ src/channel/channel_ref.rs
@@ -1,9 +1,11 @@
 use std::cell::UnsafeCell;
+use std::marker::PhantomData;
 use std::mem::MaybeUninit;
 use std::sync::atomic::{
     AtomicBool,
     Ordering::{Acquire, Relaxed, Release},
 };
+use std::thread::{self, Thread};
 
 pub struct Channel<T> {
     message: UnsafeCell<MaybeUninit<T>>,
@@ -22,23 +24,35 @@ impl<T> Channel<T> {
 
     pub fn split<'a>(&'a mut self) -> (Sender<'a, T>, Receiver<'a, T>) {
         *self = Self::new();
-        (Sender { channel: self }, Receiver { channel: self })
+        (
+            Sender {
+                channel: self,
+                receiving_thread: thread::current(),
+            },
+            Receiver {
+                channel: self,
+                _no_send: PhantomData,
+            },
+        )
     }
 }
 
 pub struct Sender<'a, T> {
     channel: &'a Channel<T>,
+    receiving_thread: Thread,
 }
 
 impl<T> Sender<'_, T> {
     pub fn send(self, message: T) {
         unsafe { (*self.channel.message.get()).write(message) };
         self.channel.ready.store(true, Release);
+        self.receiving_thread.unpark();
     }
 }
 
 pub struct Receiver<'a, T> {
     channel: &'a Channel<T>,
+    _no_send: PhantomData<*const ()>,
 }
 
 impl<T> Receiver<'_, T> {
@@ -47,8 +61,8 @@ impl<T> Receiver<'_, T> {
     }
 
     pub fn receive(self) -> T {
-        if !self.channel.ready.swap(false, Acquire) {
-            panic!("no message available");
+        while !self.channel.ready.swap(false, Acquire) {
+            thread::park();
         }
 
         unsafe { (*self.channel.message.get()).assume_init_read() }
