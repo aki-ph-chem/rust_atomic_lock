f4d55b063fbd9f881f164ee693168d7decd697fa
aki <aki@mymail.com>
2024-08-14 23:07:33 +0900

    とりあえずWeak<T>によるArc<T>を実装した

--- src/arc/arc_weak.rs
+++ src/arc/arc_weak.rs
@@ -31,6 +31,62 @@ impl<T> Arc<T> {
             },
         }
     }
+
+    pub fn get_mut(arc: &mut Self) -> Option<&mut T> {
+        if arc.weak.data().alloc_ref_count.load(Relaxed) == 1 {
+            fence(Acquire);
+            // 安全性: Archは一つしかなく、weakポインタはないので
+            // 他の何もデータにアクセスできない。
+            // そのArcに対してこのスレッドが排他アクセス権を持っている
+            let arc_data = unsafe { arc.weak.ptr.as_mut() };
+            let option = arc_data.data.get_mut();
+            // Arcがおるので、データがまだ利用できる
+            // のでここではpanicしない
+            let data = option.as_mut().unwrap();
+            Some(data)
+        } else {
+            None
+        }
+    }
+
+    /// Arc<T>からWeak<T>に変換
+    pub fn downgrade(arc: &Self) -> Weak<T> {
+        arc.weak.clone()
+    }
+}
+
+impl<T> Deref for Arc<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        let ptr = self.weak.data().data.get();
+        // 安全性: データに対するArcがあるため
+        // データは存在し、共有されているかもしれない
+        unsafe { (*ptr).as_ref().unwrap() }
+    }
+}
+
+impl<T> Clone for Arc<T> {
+    fn clone(&self) -> Self {
+        let weak = self.weak.clone();
+        if weak.data().data_ref_count.fetch_add(1, Relaxed) > usize::MAX / 2 {
+            std::process::abort();
+        }
+        Arc { weak }
+    }
+}
+
+impl<T> Drop for Arc<T> {
+    fn drop(&mut self) {
+        if self.weak.data().data_ref_count.fetch_sub(1, Release) == 1 {
+            fence(Acquire);
+            let ptr = self.weak.data().data.get();
+            // 安全性: データへの参照カウントはゼロで
+            // 他からアクセスされることはない
+            unsafe {
+                (*ptr) = None;
+            }
+        }
+    }
 }
 
 pub struct Weak<T> {
@@ -41,7 +97,87 @@ impl<T> Weak<T> {
     fn data(&self) -> &ArcData<T> {
         unsafe { self.ptr.as_ref() }
     }
+
+    /// Weak<T> からArc<T>に変換
+    pub fn upgrade(&self) -> Option<Arc<T>> {
+        let mut n = self.data().data_ref_count.load(Relaxed);
+        loop {
+            if n == 0 {
+                return None;
+            }
+            assert!(n < usize::MAX);
+            if let Err(e) =
+                self.data()
+                    .data_ref_count
+                    .compare_exchange_weak(n, n + 1, Relaxed, Relaxed)
+            {
+                n = e;
+                continue;
+            }
+            return Some(Arc { weak: self.clone() });
+        }
+    }
 }
 
 unsafe impl<T> Send for Weak<T> where T: Send + Sync {}
 unsafe impl<T> Sync for Weak<T> where T: Send + Sync {}
+
+impl<T> Clone for Weak<T> {
+    fn clone(&self) -> Self {
+        if self.data().alloc_ref_count.fetch_add(1, Relaxed) > usize::MAX / 2 {
+            std::process::abort();
+        }
+        Weak { ptr: self.ptr }
+    }
+}
+
+impl<T> Drop for Weak<T> {
+    fn drop(&mut self) {
+        if self.data().alloc_ref_count.fetch_sub(1, Release) == 1 {
+            fence(Acquire);
+            unsafe {
+                drop(Box::from_raw(self.ptr.as_ptr()));
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::thread;
+
+    #[test]
+    fn test_arc() {
+        static NUM_DROPS: AtomicUsize = AtomicUsize::new(0);
+
+        struct DetectDrop;
+        impl Drop for DetectDrop {
+            fn drop(&mut self) {
+                NUM_DROPS.fetch_add(1, Relaxed);
+            }
+        }
+
+        // この時点ではWeakはアップグレード可能
+        let x = Arc::new(("Hello", DetectDrop));
+        let y = Arc::downgrade(&x);
+        let z = Arc::downgrade(&x);
+
+        let t = thread::spawn(move || {
+            let y = y.upgrade().unwrap();
+            assert_eq!(y.0, "Hello");
+        });
+        assert_eq!(x.0, "Hello");
+        t.join().unwrap();
+
+        // データはまだdropされていないはずなので、Weakはアップグレード可能
+        assert_eq!(NUM_DROPS.load(Relaxed), 0);
+        assert!(z.upgrade().is_some());
+
+        drop(x);
+
+        // データはdropされているので、Weakはアップグレード不可能
+        assert_eq!(NUM_DROPS.load(Relaxed), 1);
+        assert!(z.upgrade().is_none());
+    }
+}
