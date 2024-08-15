b701bdff259492e30772accc45abc71b59c85c92
aki <aki@mymail.com>
2024-08-16 04:39:05 +0900

    リード・モディファイ

--- src/cpu/atomic.rs
+++ src/cpu/atomic.rs
@@ -11,3 +11,17 @@ pub fn store(x: &AtomicI32) {
 pub fn load(x: &AtomicI32) -> i32 {
     x.load(Relaxed)
 }
+
+// リード・モディファイ
+
+// 値を返さない(addl命令)
+#[no_mangle]
+pub fn add_ten(x: &AtomicI32) {
+    x.fetch_add(10, Relaxed);
+}
+
+// 値を返す(xaddl命令)
+#[no_mangle]
+pub fn add_ten_i32(x: &AtomicI32) -> i32 {
+    x.fetch_add(10, Relaxed)
+}
