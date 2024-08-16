2b5e43b2ca66b6f456a8e3c2cd6c92469eca8e91
aki <aki@mymail.com>
2024-08-16 15:22:40 +0900

    10億回のロードにおける別スレッドからのアクセスによる影響

---
+++ src/ss_7/load_billion_thread.rs
@@ -0,0 +1,28 @@
+use std::hint::black_box;
+use std::sync::atomic::{AtomicU64, Ordering::Relaxed};
+use std::thread;
+use std::time::Instant;
+
+static A: AtomicU64 = AtomicU64::new(0);
+
+// 関数をブラックボックス化して最適化でループが削られないようにする
+fn load_billion_times_black_box() {
+    let num_iter = 1_000_000_000;
+    for _ in 0..num_iter {
+        black_box(A.load(Relaxed));
+    }
+}
+
+fn main() {
+    black_box(&A);
+
+    // 別スレッドからAをロード
+    thread::spawn(|| loop {
+        black_box(A.load(Relaxed));
+    });
+
+    let start = Instant::now();
+    load_billion_times_black_box();
+    // intel core i7-13700で約 200 ms
+    println!("{:?}", start.elapsed());
+}
