517dd765cbed1c4987a5fbe09aaede94f8951abf
aki <aki@mymail.com>
2024-08-21 00:13:53 +0900

    pthread_join()

---
+++ posix_pthreads_c/thread_join_mwe.c
@@ -0,0 +1,23 @@
+#include <pthread.h>
+#include <unistd.h>
+#include <stdio.h>
+
+void* worker(void* param) {
+    for(int i = 0; i < 3; ++i) {
+        puts((const char*) param);
+        sleep(1);
+    }
+
+    return (void*)"done";
+}
+
+int main(void) {
+    pthread_t t;
+    void* result;
+    pthread_create(&t, NULL, worker, (void*) "I am a worker!");
+    // スレッドtが終了するまで待機して、worker()からの返り値を受け取る
+    pthread_join(t, &result);
+    puts((const char*) result);
+
+    return 0;
+}
